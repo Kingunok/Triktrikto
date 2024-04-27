3abeeefa404c555860798356fca187d83f03d176
Kingunok <114995588+Kingunok@users.noreply.github.com>
2024-04-28 02:03:05 +0530

    Update util.py

--- app/util.py
+++ app/util.py
@@ -1,3 +1,26 @@
+from os import environ
+from typing import Dict, Optional
+
+
+class TokenParser:
+    def __init__(self, config_file: Optional[str] = None):
+        self.tokens = {}
+        self.config_file = config_file
+
+    def parse_from_env(self) -> Dict[int, str]:
+        self.tokens = dict(
+            (c + 1, t)
+            for c, (_, t) in enumerate(
+                filter(
+                    lambda n: n[0].startswith("MULTI_TOKEN"), sorted(environ.items())
+                )
+            )
+        )
+        return self.tokens
+
+
+
+
 def get_file_name(message):
     if message.file.name:
         return message.file.name.replace('\n', ' ')
