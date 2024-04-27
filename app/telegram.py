aad9e01e94902dcaee838810b545f26cc3e739c2
Kingunok <114995588+Kingunok@users.noreply.github.com>
2024-04-28 02:14:21 +0530

    Update telegram.py

--- app/telegram.py
+++ app/telegram.py
@@ -1,21 +1,49 @@
 import math
 import logging
 import asyncio
+from ..vars import Var
+from pyrogram import Client
+from utils.config_parser import TokenParser
+from botCode import multi_clients, work_loads, StreamBot
 
-from telethon import TelegramClient, utils
-from telethon.sessions import StringSession
-
-class Client(TelegramClient):
-
-    def __init__(self, session_string, *args, **kwargs):
-        super().__init__(StringSession(session_string), *args, **kwargs)
-        self.log = logging.getLogger(__name__)
-    
+class CustomClient(Client):
+    async def initialize_clients(self):
+        multi_clients[0] = StreamBot
+        work_loads[0] = 0
+        all_tokens = TokenParser().parse_from_env()
+        if not all_tokens:
+            print("No additional clients found, using default client")
+            return
+        
+        async def start_client(client_id, token):
+            try:
+                print(f"Starting - Client {client_id}")
+                if client_id == len(all_tokens):
+                    await asyncio.sleep(2)
+                    print("This will take some time, please wait...")
+                client = await Client(
+                    name=":memory:",
+                    api_id=Var.API_ID,
+                    api_hash=Var.API_HASH,
+                    bot_token=token,
+                    sleep_threshold=Var.SLEEP_THRESHOLD,
+                    no_updates=True,
+                ).start()
+                work_loads[client_id] = 0
+                return client_id, client
+            except Exception:
+                logging.error(f"Failed starting Client - {client_id} Error:", exc_info=True)
+        
+        clients = await asyncio.gather(*[start_client(i, token) for i, token in all_tokens.items()])
+        multi_clients.update(dict(clients))
+        if len(multi_clients) != 1:
+            Var.MULTI_CLIENT = True
+            print("Multi-Client Mode Enabled")
+        else:
+            print("No additional clients were initialized, using default client")
 
     async def download(self, file, file_size, offset, limit):
-        # part_size_kb = utils.get_appropriated_part_size(file_size)
-        # part_size = int(part_size_kb * 1024)
-        part_size = 512*1024
+        part_size = 512 * 1024
         first_part_cut = offset % part_size
         first_part = math.floor(offset / part_size)
         last_part_cut = part_size - (limit % part_size)
@@ -23,7 +51,14 @@ class Client(TelegramClient):
         part_count = math.ceil(file_size / part_size)
         part = first_part
         try:
-            async for chunk in self.iter_download(file, offset=first_part * part_size, request_size=part_size):
+            while True:
+                chunk = await self.download_media(
+                    file,
+                    offset=first_part * part_size,
+                    file_size=part_size,
+                    part_size=part_size,
+                    progress=None
+                )
                 if part == first_part:
                     yield chunk[first_part_cut:]
                 elif part == last_part:
@@ -32,9 +67,8 @@ class Client(TelegramClient):
                     yield chunk
                 self.log.debug(f"Part {part}/{last_part} (total {part_count}) served!")
                 part += 1
-            self.log.debug("serving finished")
-        except (GeneratorExit, StopAsyncIteration, asyncio.CancelledError):
-            self.log.debug("file serve interrupted")
-            raise
-        except Exception:
-            self.log.debug("file serve errored", exc_info=True)
+        except FloodWait as e:
+            self.log.warning(f"Flood wait: {e}")
+            await asyncio.sleep(e.x)
+        except Exception as e:
+            self.log.error(f"An error occurred: {e}", exc_info=True)
