46c9bf0123ccca7efaf155d58297c6df368c5ab0
Kingunok <114995588+Kingunok@users.noreply.github.com>
2024-04-28 01:48:40 +0530

    Update __main__.py

--- app/__main__.py
+++ app/__main__.py
@@ -12,7 +12,7 @@ from .config import debug
 
 
 logging.basicConfig(level=logging.DEBUG if debug else logging.INFO)
-logging.getLogger("telethon").setLevel(logging.INFO if debug else logging.ERROR)
+logging.getLogger("pyrogram").setLevel(logging.INFO if debug else logging.ERROR)
 logging.getLogger("aiohttp").setLevel(logging.INFO if debug else logging.ERROR)
 
 
