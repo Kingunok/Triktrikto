e9a38abf3cde2ad9a94f9faafcfa131d14c9f4a5
Kingunok <114995588+Kingunok@users.noreply.github.com>
2024-04-28 10:09:29 +0530

    Update botCode.py

--- app/botCode.py
+++ app/botCode.py
@@ -3,7 +3,7 @@ from .config import api_id, api_hash, bot_token,workers,sleep_threshold
 import pyromod.listen
 from os import getcwd
 
-bot = Client('bot', api_id, api_hash, bot_token=bot_token, workers=worker,sleep_threshold=sleep_threshold)
+bot = Client('bot', api_id, api_hash, bot_token=bot_token, workers=workers,sleep_threshold=sleep_threshold)
 
 @bot.on_message(filters.command('start'))
 async def send_welcome(client, message):
